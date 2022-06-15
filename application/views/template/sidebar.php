959f1c38e95f875b11da6a257fabd67b0e88382b
mfathonyramdhan <mfathonyramdhan@gmail.com>
2022-06-15 23:00:13 +0700

    fix href di logo sidebar
    -> diarahkan ke page dashboard admin

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -5,7 +5,7 @@
                 <div class="sidebar-header">
                     <div class="d-flex justify-content-between">
                         <div class="logo">
-                            <a href="index.html"><img src="<?= base_url('assets/images/') ?>logo/logo.png" alt="Logo" srcset="" /></a>
+                            <a href="<?= base_url('admin') ?>"><img src="<?= base_url('assets/images/') ?>logo/logo.png" alt="Logo" srcset="" /></a>
                         </div>
                         <div class="toggler">
                             <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
